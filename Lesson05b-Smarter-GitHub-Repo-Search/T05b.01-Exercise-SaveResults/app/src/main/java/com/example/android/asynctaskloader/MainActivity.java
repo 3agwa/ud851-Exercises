1799252392cfc753d2ef0adaf29ae8d74bfadc48
3agwa <a.mounir.112@gmail.com>
2019-12-31 19:50:40 +0200

    saved the url link and the json result using onSaveInstanceState to show the data when rotating the device

--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -17,6 +17,7 @@ package com.example.android.asynctaskloader;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,14 +28,17 @@ import android.widget.TextView;
 
 import com.example.android.asynctaskloader.utilities.NetworkUtils;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a static final key to store the query's URL
-
-    // TODO (2) Create a static final key to store the search's raw JSON
+    // COMPLETED (1) Create a static final key to store the query's URL
+    private static final String queryURL = "query";
+    // COMPLETED (2) Create a static final key to store the search's raw JSON
+    private static final String rawSearchJson = "result";
 
     private EditText mSearchBoxEditText;
 
@@ -59,7 +63,19 @@ public class MainActivity extends AppCompatActivity {
 
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
-        // TODO (9) If the savedInstanceState bundle is not null, set the text of the URL and search results TextView respectively
+        // COMPLETED (9) If the savedInstanceState bundle is not null, set the text of the URL and search results TextView respectively
+
+        if (savedInstanceState != null)
+        {
+            if (savedInstanceState.containsKey(queryURL))
+            {
+                mUrlDisplayTextView.setText(savedInstanceState.getString(queryURL));
+            }
+            if (savedInstanceState.containsKey(rawSearchJson))
+            {
+                mSearchResultsTextView.setText(savedInstanceState.getString(rawSearchJson));
+            }
+        }
     }
 
     /**
@@ -151,13 +167,23 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // TODO (3) Override onSaveInstanceState to persist data across Activity recreation
-    // Do the following steps within onSaveInstanceState
-    // TODO (4) Make sure super.onSaveInstanceState is called before doing anything else
+    // COMPLETED (3) Override onSaveInstanceState to persist data across Activity recreation
 
-    // TODO (5) Put the contents of the TextView that contains our URL into a variable
-    // TODO (6) Using the key for the query URL, put the string in the outState Bundle
+    @Override
+    protected void onSaveInstanceState(Bundle outState)
+    {
+        // Do the following steps within onSaveInstanceState
+        // COMPLETED (4) Make sure super.onSaveInstanceState is called before doing anything else
+        super.onSaveInstanceState(outState);
+        // COMPLETED (5) Put the contents of the TextView that contains our URL into a variable
+        String urlContent = mUrlDisplayTextView.getText().toString();
+        // COMPLETED (6) Using the key for the query URL, put the string in the outState Bundle
+        outState.putString(queryURL, urlContent);
+        // COMPLETED (7) Put the contents of the TextView that contains our raw JSON search results into a variable
+        String rawJSONContent = mSearchResultsTextView.getText().toString();
+        // COMPLETED (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
+        outState.putString(rawSearchJson, rawJSONContent);
+    }
 
-    // TODO (7) Put the contents of the TextView that contains our raw JSON search results into a variable
-    // TODO (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
+
 }
